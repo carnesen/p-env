bacba6bf68e96bc687d039ab7e755235f73bb85a
Chris Arnesen <chris.arnesen@gmail.com>
2022-04-06 17:27:23 -0500

    WIP String choices

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+/** Value displayed in logs when a field is "secret" */
+export const P_ENV_REDACTED_VALUE = '<redacted>';
--- src/field-types/string.ts
+++ src/field-types/string.ts
@@ -4,14 +4,28 @@ import {
 	PEnvFieldTypeConfig,
 } from '../abstract-field-type';
 import { pEnvFailure, PEnvResult, pEnvSuccess } from '../result';
+import { P_ENV_REDACTED_VALUE } from '../constants';
 
-export interface PEnvStringConfig extends PEnvFieldTypeConfig<string> {
+type AnyChoices = undefined | string[] | readonly string[];
+
+type ParsedFromChoices<Choices extends AnyChoices> =
+	Choices extends readonly string[]
+		? Choices[number]
+		: Choices extends string[]
+		? Choices[number]
+		: string;
+
+export interface PEnvStringConfig<Choices extends AnyChoices>
+	extends PEnvFieldTypeConfig<ParsedFromChoices<Choices>> {
+	choices?: Choices;
 	/** If provided, a value longer than this is considered invalid */
 	maxLength?: number;
 }
 
-export class PEnvString extends PEnvAbstractFieldType<string> {
-	private constructor(readonly config: PEnvStringConfig) {
+export class PEnvString<
+	Choices extends AnyChoices,
+> extends PEnvAbstractFieldType<ParsedFromChoices<Choices>> {
+	private constructor(readonly config: PEnvStringConfig<Choices>) {
 		super(config);
 		if (config.maxLength) {
 			if (config.maxLength < 0) {
@@ -25,18 +39,31 @@ export class PEnvString extends PEnvAbstractFieldType<string> {
 		}
 	}
 
-	safeParse(envValue: string): PEnvResult<string> {
+	safeParse(envValue: string): PEnvResult<ParsedFromChoices<Choices>> {
 		if (
 			typeof this.config.maxLength === 'number' &&
 			envValue.length > this.config.maxLength
 		) {
 			return pEnvFailure(`is longer than maxLength=${this.config.maxLength}`);
 		}
-		return pEnvSuccess(envValue);
+		if (this.config.choices) {
+			if (!this.config.choices.includes(envValue)) {
+				return pEnvFailure(
+					`is not one of allowed values: ${
+						this.config.secret
+							? P_ENV_REDACTED_VALUE
+							: this.config.choices.join(', ')
+					}`,
+				);
+			}
+		}
+		return pEnvSuccess(envValue) as PEnvResult<ParsedFromChoices<Choices>>;
 	}
 
 	/** Factory for `string`-valued environment variables */
-	static create(options: PEnvStringConfig): PEnvString {
+	static create<CreateChoices extends AnyChoices>(
+		options: PEnvStringConfig<CreateChoices>,
+	): PEnvString<CreateChoices> {
 		return new PEnvString(options);
 	}
 }
